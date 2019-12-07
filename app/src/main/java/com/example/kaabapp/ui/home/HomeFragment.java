1b98d555db87cb3652b374ae3b7b8dae4b77bcd1
denniscetina <denniscetina@users.noreply.github.com>
2019-12-07 15:30:24 -0500

    Cambios

--- app/src/main/java/com/example/kaabapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/kaabapp/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.kaabapp.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,28 +10,108 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.kaabapp.Anime;
 import com.example.kaabapp.R;
+import com.example.kaabapp.RecyclerViewAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private final String JSON_URLG = "https://gist.githubusercontent.com/aws1994/f583d54e5af8e56173492d3f60dd5ebf/raw/c7796ba51d5a0d37fc756cf0fd14e54434c547bc/anime.json" ;
+    private final String JSON_URL="http://192.168.1.10/reciclaxd.json";
+    private JsonArrayRequest request ;
+    private RequestQueue requestQueue ;
+    private List<Anime> lstAnime ;
+    private RecyclerView recyclerView ;
+    final FragmentActivity fragmentActivity = getActivity();
+    Context thiscontext;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+                             final ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        lstAnime = new ArrayList<>() ;
+        recyclerView = root.findViewById(R.id.recyclerviewid);
+        //jsonrequest();
+        this.thiscontext = thiscontext;
 
-        homeViewModel.getText().observe(this, new Observer<String>() {
+        request = new JsonArrayRequest(JSON_URL, new Response.Listener<JSONArray>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onResponse(JSONArray response) {
+
+                JSONObject jsonObject  = null ;
+
+                for (int i = 0 ; i < response.length(); i++ ) {
+
+
+                    try {
+                        jsonObject = response.getJSONObject(i) ;
+                        Anime anime = new Anime() ;
+                        anime.setName(jsonObject.getString("name"));
+                        anime.setDescription(jsonObject.getString("description"));
+                        anime.setRating(jsonObject.getString("Rating"));
+                        anime.setCategorie(jsonObject.getString("categorie"));
+                        anime.setNb_episode(jsonObject.getInt("episode"));
+                        anime.setStudio(jsonObject.getString("studio"));
+                        anime.setImage_url(jsonObject.getString("img"));
+                        lstAnime.add(anime);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+                RecyclerViewAdapter myadapter = new RecyclerViewAdapter(getActivity(),lstAnime) ;
+                recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                recyclerView.setAdapter(myadapter);
+                //setuprecyclerview(lstAnime);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
             }
         });
+
+
+        requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        requestQueue.add(request) ;
+
+
         return root;
+    }
+    private void jsonrequest() {
+
+
+    }
+
+    private void setuprecyclerview(List<Anime> lstAnime) {
+
+
+
+
+
+
     }
 }
